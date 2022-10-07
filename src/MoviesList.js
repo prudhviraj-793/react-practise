c4efde79d27d786e548b1cc0795d907ed4337900
Prudhvi Raj <prudhvi@123.com>
2022-10-07 20:02:43 +0530

    error

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -2,33 +2,41 @@ import { useState } from "react";
 import Movie from "./Movie";
 
 function MoviesList() {
-
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('')
 
   async function fetchHandler(e) {
-    setIsLoading(true)
+    setIsLoading(true);
     e.preventDefault();
-    let response = await fetch("https://swapi.dev/api/films/");
-    let data = await response.json();
-    let movies = data.results;
+    try {
+      let response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("something went wrong...!");
+      }
+      let data = await response.json();
+      let movies = data.results;
 
-    let tranformedData = movies.map((movie) => {
-      return {
-        id: movie["episode_id"],
-        title: movie.title,
-        openingText: movie["opening_crawl"],
-        releaseDate: movie["release_date"],
-      };
-    });
+      let tranformedData = movies.map((movie) => {
+        return {
+          id: movie["episode_id"],
+          title: movie.title,
+          openingText: movie["opening_crawl"],
+          releaseDate: movie["release_date"],
+        };
+      });
+      setMovies(tranformedData);
+    } catch (error) {
+      setError(error.message);
+    }
 
-    setMovies(tranformedData);
-    setIsLoading(false)
+    setIsLoading(false);
   }
   return (
     <div>
       <button onClick={fetchHandler}>Fetch</button>
       {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
       <Movie movieList={movies} />
     </div>
   );
